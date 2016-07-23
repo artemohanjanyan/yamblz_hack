06d6f8f7b88d5b506d9aa01447a7071a3878646e
Artem Ohanjanyan <artemohanjanyan@gmail.com>
2016-07-23 19:54:03 +0300

    translation with animation

--- app/src/main/java/yamblz/hack/learning/ui/tasks/cards/CardsFragment.java
+++ app/src/main/java/yamblz/hack/learning/ui/tasks/cards/CardsFragment.java
@@ -1,5 +1,6 @@
 package yamblz.hack.learning.ui.tasks.cards;
 
+import android.animation.AnimatorSet;
 import android.animation.ObjectAnimator;
 import android.content.Intent;
 import android.os.Bundle;
@@ -34,6 +35,7 @@ public class CardsFragment extends Fragment implements CardsLayout.Listener,
     private View button;
     private ImageView learnCount;
     private ProgressBar progressBar;
+    private TextView translationView;
 
     private List<WordPair> wordPairList;
     private int currentWord = 0;
@@ -51,6 +53,7 @@ public class CardsFragment extends Fragment implements CardsLayout.Listener,
         button = cardsLayout.findViewById(R.id.fragment_cards_button);
         learnCount = (ImageView) cardsLayout.findViewById(R.id.fragment_cards_learn_count);
         progressBar = (ProgressBar) cardsLayout.findViewById(R.id.fragment_cards_progress_bar);
+        translationView = (TextView) cardsLayout.findViewById(R.id.fragment_cards_text_translation);
 
         return cardsLayout;
     }
@@ -93,6 +96,28 @@ public class CardsFragment extends Fragment implements CardsLayout.Listener,
         }
         learnCount.setImageResource(drawableId);
         progressBar.setProgress(currentWord);
+        translationView.setText(wordPairList.get(currentWord).getSecondWord());
+        textView.setTranslationY(0);
+        translationView.setTranslationY(0);
+        translationView.setAlpha(0);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                ObjectAnimator animator1 = ObjectAnimator
+                        .ofFloat(textView, "translationY", 0, -48)
+                        .setDuration(300);
+                ObjectAnimator animator2 = ObjectAnimator
+                        .ofFloat(translationView, "translationY", 0, 48)
+                        .setDuration(300);
+                ObjectAnimator animator3 = ObjectAnimator
+                        .ofFloat(translationView, "alpha", 0, 1)
+                        .setDuration(300);
+                AnimatorSet animatorSet = new AnimatorSet();
+                animatorSet.playTogether(animator1, animator2, animator3);
+                animatorSet.start();
+                button.setOnClickListener(null);
+            }
+        });
     }
 
     @Override
@@ -127,7 +152,6 @@ public class CardsFragment extends Fragment implements CardsLayout.Listener,
     @Override
     public void onLoadFinished(Loader<List<WordPair>> loader, List<WordPair> data) {
         wordPairList = data;
-        Log.d("qq", "" + wordPairList.size());
         button.setClickable(true);
         inflateData();
     }
